bd5a1abdc0cc18be6ffdb63eba2864ae75e57928
fitri <fitriakmaliah1304@gmail.com>
2024-06-02 22:41:53 +0700

    commit kesatu

---
+++ register.php
@@ -0,0 +1,24 @@
+<?php
+    if(isset($_SESSION['email'])){
+        header("Location: index.php");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Register</h2>
+    <form method="POST" action="./register_proccess.php">
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email"><br><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password"><br><br>
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>  
